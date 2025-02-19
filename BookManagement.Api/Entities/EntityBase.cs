b53fc33555cc0d6694dcd054781ba038abbad493
FIS ES <>
2025-02-19 23:25:05 +0700

    update entitybase

--- BookManagement.Api/Entities/EntityBase.cs
+++ BookManagement.Api/Entities/EntityBase.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BookManagement.Api.Entities
 {
 	public class EntityBase
 	{
-		public long Id { get; set; }
+        [Key]  // Đánh dấu thuộc tính là khóa chính
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
 		public DateTime CreateAt { get; set; }
 		public string CreateBy { get; set; } = string.Empty;
         public DateTime UpdateAt { get; set; }
